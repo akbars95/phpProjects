b0e3150357c7e1e7df4f47b7c8efb3eb6cf1260c
akbars95 <mynzat.dmitrii@gmail.com>
2015-08-04 07:16:53 +0300

    - create project

---
+++ testing/com.mtsmda.testing.pieces/Pieces.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: MTSMDA
+ * Date: 04.08.15
+ * Time: 6:56
+ * To change this template use File | Settings | File Templates.
+ */
+class Pieces
+{
+    public static function getHeader(){
+
+    }
+
+}
